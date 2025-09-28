76e496c4f22a7638598357ddf72dbdd0110f4228
Huy Hoang <boydaihungst@gmail.com>
2025-09-28 13:48:00 +0700

    fix: better handle cwd

--- main.lua
+++ main.lua
@@ -5,21 +5,27 @@ local PackageName = "hover-after-moved"
 local M = {}
 
 function M:setup()
-	ps.sub("move", function(payload)
-		if not payload then
-			return
-		end
-
-		if payload.items and #payload.items > 0 then
-			if cx.active.current.cwd and cx.active.current.cwd == payload.items[1].to.parent then
-				ya.emit("reveal", {
-					payload.items[1].to,
-					no_dummy = true,
-					raw = true,
-					tab = cx.active.id.value,
-				})
+	ps.sub("@yank", function(_)
+		ps.unsub("move")
+		ps.sub("move", function(payload)
+			if not payload then
+				ps.unsub("move")
+				return
 			end
-		end
+
+			if payload.items and #payload.items > 0 then
+				if cx.active.current.cwd and cx.active.current.cwd == payload.items[1].to.parent then
+					ya.emit("reveal", {
+						payload.items[1].to,
+						no_dummy = true,
+						raw = true,
+						tab = cx.active.id.value,
+					})
+					return
+				end
+			end
+			ps.unsub("move")
+		end)
 	end)
 end
 
