d58f01fce44889ed3991b5ea58387d616ec2c749
Huy Hoang <boydaihungst@gmail.com>
2025-12-26 07:33:41 +0700

    feat: support yank/copy/duplicate action for yazi > 25.9.15

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hover-after-moved
 
-Hover over a file after moved it to another folder.
+Hover over a file after moved/copied it to another folder.
 
 > [!IMPORTANT]
 > Minimum version: yazi v25.5.31.
@@ -8,6 +8,9 @@ Hover over a file after moved it to another folder.
 ## Requirements
 
 - [yazi >= 25.5.31](https://github.com/sxyazi/yazi)
+
+> [!NOTE]
+> Yazi >= 25.9.15 (use `yazi --debug`) also supports copy/yank action.
 
 ## Installation
 
--- main.lua
+++ main.lua
@@ -7,6 +7,26 @@ local M = {}
 function M:setup()
 	ps.sub("@yank", function(_)
 		ps.unsub("move")
+		ps.sub("duplicate", function(payload)
+			ya.err("duplicate", payload)
+			if not payload then
+				ps.unsub("duplicate")
+				return
+			end
+			if payload.items and #payload.items > 0 then
+				if cx.active.current.cwd and cx.active.current.cwd == payload.items[1].to.parent then
+					ya.emit("reveal", {
+						payload.items[1].to,
+						no_dummy = true,
+						raw = true,
+						tab = cx.active.id.value,
+					})
+					return
+				end
+			end
+			ps.unsub("duplicate")
+		end)
+
 		ps.sub("move", function(payload)
 			if not payload then
 				ps.unsub("move")
