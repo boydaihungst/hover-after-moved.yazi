c2e90df9278538741a3793d59f78af50e33bdff7
Huy Hoang <boydaihungst@gmail.com>
2026-01-06 13:08:43 +0700

    fix: search mode

--- main.lua
+++ main.lua
@@ -9,13 +9,17 @@ function M:setup()
 		ps.unsub("move")
 		ps.unsub("duplicate")
 		ps.sub("duplicate", function(payload)
-			ya.err("duplicate", payload)
 			if not payload then
 				ps.unsub("duplicate")
 				return
 			end
 			if payload.items and #payload.items > 0 then
-				if cx.active.current.cwd and cx.active.current.cwd == payload.items[1].to.parent then
+				local is_virtual = Url(cx.active.current.cwd).scheme and Url(cx.active.current.cwd).scheme.is_virtual
+				if
+					cx.active.current.cwd
+					and Url(is_virtual and cx.active.current.cwd or cx.active.current.cwd.path)
+						== payload.items[1].to.parent
+				then
 					ya.emit("reveal", {
 						payload.items[1].to,
 						no_dummy = true,
@@ -35,7 +39,12 @@ function M:setup()
 			end
 
 			if payload.items and #payload.items > 0 then
-				if cx.active.current.cwd and cx.active.current.cwd == payload.items[1].to.parent then
+				local is_virtual = Url(cx.active.current.cwd).scheme and Url(cx.active.current.cwd).scheme.is_virtual
+				if
+					cx.active.current.cwd
+					and Url(is_virtual and cx.active.current.cwd or cx.active.current.cwd.path)
+						== payload.items[1].to.parent
+				then
 					ya.emit("reveal", {
 						payload.items[1].to,
 						no_dummy = true,
